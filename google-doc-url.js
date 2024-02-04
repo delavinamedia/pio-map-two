e668c88fec376301102ffccd2890092a4f4fc47c
delavinamedia <37664292+delavinamedia@users.noreply.github.com>
2024-02-04 11:45:56 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/16Apu43CU9MlLe-tQ9RQYVaHzIO9kpQ14x_f1gr-5Amk/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1OWagAfnGSfLMq88gOq6UmDugm5QWpgX-J8U1DhhN5jo/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAALRA_a64Mvy_1viredVbg_QbThQZA5oI';
